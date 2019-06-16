1af8504ab8aa07338f625582faa60e897b7e575a
Anwesh43 <anweshthecool0@gmail.com>
2019-06-16 15:39:44 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -3,7 +3,32 @@ const maxNodes : number = 20
 const hgap : number = h / (maxNodes)
 const wgap : number = (w / 2) / (maxNodes)
 const r : number = Math.min(wgap, hgap) / 5
+const scGap : number = 0.1
 var data = 0
+
+class State {
+
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
 
 class BinaryTreeNode {
 
